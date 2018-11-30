17984d3532c28f512380d78ec533d4c380f7e9e1
Jared Tanner <Deraj21@gmail.com>
2018-11-29 18:20:13 -0700

    Updates

---
+++ precache-manifest.ddd0b36c3136863dd451780c104e5c78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jaredmtanner-portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d116308cac6117c3a37a",
+    "url": "/jaredmtanner-portfolio/static/js/runtime~main.d116308c.js"
+  },
+  {
+    "revision": "d81c0c29d47d68d08a8d",
+    "url": "/jaredmtanner-portfolio/static/js/main.d81c0c29.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/jaredmtanner-portfolio/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "d81c0c29d47d68d08a8d",
+    "url": "/jaredmtanner-portfolio/static/css/main.4abf2f2f.chunk.css"
+  },
+  {
+    "revision": "7ffb2dfdf7be97d5cbb24e10798c37b0",
+    "url": "/jaredmtanner-portfolio/index.html"
+  }
+];
